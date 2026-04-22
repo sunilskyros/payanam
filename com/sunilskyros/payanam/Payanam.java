e1ae71060b06d8629d80725aa724a9300df58a1d
sunilkumar <sunilkumarfsd12@gmail.com>
2026-04-22 14:06:40 +0530

    Changes

--- com/sunilskyros/payanam/Payanam.java
+++ com/sunilskyros/payanam/Payanam.java
@@ -1,10 +1,14 @@
 package com.sunilskyros.payanam;
 
+import com.sunilskyros.payanam.features.signup.SignUpView;
+
+import java.sql.Connection;
+
 public class Main {
 
     public static final int VERSION_NO = 1;
     public static final String VERSION_NAME = "0.0.1";
     public static void main(String[] args) {
-
+        new SignUpView().init();
     }
 }
